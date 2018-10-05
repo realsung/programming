539009e0b1f6a2c3d14bab7adccdf3e102faa806
realsung <sungjun0208@naver.com>
2018-10-05 09:41:20 +0900

    add rot13 & webParsing python code

---
+++ Python/rot13.py
@@ -0,0 +1,30 @@
+def translate(string, key, mode):
+    translated_string = ""
+ 
+    if mode == 'decrypt':
+        key = -key
+ 
+    for char in string:
+        if char.isalpha():
+            num = ord(char)
+            num += key
+ 
+            if char.isupper():
+                if num > ord('Z'):
+                    num -= 26
+                elif num < ord('A'):
+                    num += 26
+            elif char.islower():
+                if num > ord('z'):
+                    num -= 26
+                elif num < ord('a'):
+                    num += 26
+            translated_string += chr(num)
+        else:
+            translated_string += char
+    return translated_string
+
+encrypted = 'NFY dklrghwb n nqc vvjtg Rheejal ldj' # input
+for i in range(1, 26):
+    decrypted = translate(encrypted, i, 'decrypt')
+    print ("".join(['ROT', str(i), ': ', decrypted]))
---
+++ Python/webParsing.py
@@ -0,0 +1,25 @@
+# requests_parsing.py
+#-*- coding: utf-8 -*-
+import requests
+
+request = requests.get('http://2018shell2.picoctf.com:3827/flag')
+
+# HTML 소스 가져오기
+html_parsing = request.text
+print(html_parsing)
+print("---"*30)
+
+# HTTP Header 가져오기
+header_parsing = request.headers
+print(header_parsing)
+print("---"*30)
+
+# HTTP Status 가져오기 (출력이 200이면 정상)
+http_status = request.status_code
+print(http_status)
+print("---"*30)
+
+# HTTP가 정상적인지 확인
+http_ok = request.ok
+print(http_ok)
+print("---"*30)
