fa5764a633157f76f429ed36345ec04ff13f1f39
realsung <sungjun0208@naver.com>
2018-10-28 00:40:42 +0900

    update rot13

--- Python/rot13.py
+++ Python/rot13.py
@@ -24,7 +24,7 @@ def translate(string, key, mode):
             translated_string += char
     return translated_string
 
-encrypted = '' # input
+encrypted = 'c3Vucmlu' # input
 for i in range(1, 26):
     decrypted = translate(encrypted, i, 'decrypt')
     print ("".join(['ROT', str(i), ': ', decrypted]))
